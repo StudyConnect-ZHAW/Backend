032ad105662f10d5a7d04ba16d25a237261c8052
froehseb <froehseb@students.zhaw.ch>
2025-03-28 13:07:41 +0100

    add user endpoint for documentation

---
+++ StudyConnect.API/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyConnect.API.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class BaseController : Controller
+    {
+
+    }
+}
