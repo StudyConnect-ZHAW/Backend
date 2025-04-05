dc1ebf9e32a5c630a0c4409071a418c093f256b1
Daniel Bucher <danielbucher.db@gmail.com>
2025-04-05 17:23:43 +0200

    build: updating Linux .sh commands [NOISSUE]

--- start-and-open.sh
+++ start-and-open.sh
@@ -10,16 +10,14 @@ sleep 5
 if [[ "$OSTYPE" == "darwin"* ]]; then
     open http://localhost:8080/swagger/  # macOS
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if command -v xdg-open &> /dev/null; then
-        xdg-open http://localhost:8080/swagger/ # Most Linux
-    elif command -v gnome-open &> /dev/null; then
-        gnome-open http://localhost:8080/swagger/ # Gnome Desktop
-    elif command -v kde-open &> /dev/null; then
-        kde-open http://localhost:8080/swagger/ # KDE Desktop
-    elif command -v firefox &> /dev/null; then
+    if command -v firefox &> /dev/null; then
         firefox http://localhost:8080/swagger/ # if firefox is installed
+    elif command -v chromium &> /dev/null; then
+        chromium http://localhost:8080/swagger/ # if chromium browser is installed
     elif command -v chrome &> /dev/null; then
         chrome http://localhost:8080/swagger/ # if chrome is installed
+    elif command -v xdg-open &> /dev/null; then
+        xdg-open http://localhost:8080/swagger/ # Most Linux
     else
         echo "Could not find a suitable command to open the URL on Linux."
     fi
