c5872212d5e0a7a217ca126f3c35dafe9f6d1b2f
Daniel Bucher <danielbucher.db@gmail.com>
2025-04-07 00:26:52 +0200

    test: Adding Example Unit Test [#51]

---
+++ StudyConnect.Tests/Unit/ExampleUnitTest.cs
@@ -0,0 +1,13 @@
+namespace StudyConnect.Tests;
+
+/// <summary>
+///  This is a dummy unit test class for the StudyConnect application.
+/// </summary>
+public class ExampleUnitTest
+{
+    [Fact]
+    public void DummyTest()
+    {
+        Assert.True(true);
+    }
+}
