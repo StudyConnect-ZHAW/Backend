41530b46248888e51df0bb2a174d9cac69a851dc
Newoss <sebastian.froehlicher@gmail.com>
2025-04-19 12:05:15 +0200

    new migration with UserRole Student

---
+++ StudyConnect.Data/Migrations/20250419100241_AddRoleToUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyConnect.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoleToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserRole",
+                columns: new[] { "URoleId", "Description", "Name" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), "Student is the default role with no rights.", "Student" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumn: "URoleId",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"));
+        }
+    }
+}
