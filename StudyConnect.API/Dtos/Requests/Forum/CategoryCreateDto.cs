c1a16d21b623db45f3d549301d9ad3f3e7db8c9d
sokolgle <gleb.sokolovskiy@gmail.com>
2025-04-21 19:02:29 +0200

    feat: added a request dto for forumcategory [#53]

---
+++ StudyConnect.API/Dtos/Requests/Forum/CategoryCreateDto.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudyConnect.API.Dtos.Requests.Forum;
+
+/// <summary>
+/// Data transfer object for creating a new category.
+/// </summary>
+public class CategoryCreateDto
+{
+    /// <summary>
+    /// The unique identifier for the forumcategory.
+    /// </summary>
+    [Required(ErrorMessage = "ForumCategory GUID is required.")]
+    public required Guid ForumCategoryId { get; set; }
+
+    /// <summary>
+    /// The Name of the category.
+    /// </summary>
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(255)]
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// The description of the category.
+    /// </summary>
+    [StringLength(255)]
+    public string? Description { get; set; }
+}
