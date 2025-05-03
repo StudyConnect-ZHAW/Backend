4bb12cf2735f1d4f1f9f4a7e16b70870cff2424f
sokolgle <gleb.sokolovskiy@gmail.com>
2025-05-03 16:20:21 +0200

    feat: added IPostRepository [#66]

---
+++ StudyConnect.Core/Interfaces/IPostRepository.cs
@@ -0,0 +1,41 @@
+using StudyConnect.Core.Common;
+using StudyConnect.Core.Models;
+
+namespace StudyConnect.Core.Interfaces;
+
+public interface IPostRepository
+{
+    /// <summary>
+    /// Add a Post to the database.
+    /// </summary>
+    /// <param name="post">The forumpost to be added.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<bool>> AddAsync (ForumPost? post);
+    
+    /// <summary>
+    /// Get a Post by its GUID.
+    /// </summary>
+    /// <param name="id">the unique identifier of the post.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> containing the post if found, or an error message if not.</returns>
+    Task<OperationResult<ForumPost?>> GetByIdAsync (Guid id);
+
+    /// <summary>
+    /// Get all Post of the forum.
+    /// </summary>
+    /// <returns>An <see cref="OperationResult{T}"/> containing a list of post if found, or an error message if not.</returns>
+    Task<OperationResult<IEnumerable<ForumPost>>> GetAllAsync();
+
+    /// <summary>
+    /// Update a Post by its GUID.
+    /// </summary>
+    /// <param name="id">The unique identifier of the post.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<bool>> UpdateAsync (Guid id);
+
+    /// <summary>
+    /// Delete an exitsting post by its GUID.
+    /// </summary>
+    /// <param name="id">The unique identifier of the post.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<bool>> DeleteAsync (Guid id);
+}
