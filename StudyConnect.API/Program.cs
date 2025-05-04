852f54961b516a3de5b1105dbf539ea3e4e42596
thayarag <thayarag@students.zhaw.ch>
2025-05-04 19:30:41 +0200

    fix: error code 500 #57

--- StudyConnect.API/Program.cs
+++ StudyConnect.API/Program.cs
@@ -11,7 +11,6 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<IGroupRepository, GroupRepository>();
-builder.Services.AddScoped<IGroupRepository, GroupRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
