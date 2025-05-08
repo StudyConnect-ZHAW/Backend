7d361c18328d65fc50caa83bd43f530c0f4f6491
sokolgle <gleb.sokolovskiy@gmail.com>
2025-05-08 09:25:34 +0200

    feat: added base ICommentRepository [#67]

--- StudyConnect.Core/Interfaces/ICommentRepository.cs
+++ StudyConnect.Core/Interfaces/ICommentRepository.cs
@@ -2,3 +2,44 @@ using StudyConnect.Core.Common;
 using StudyConnect.Core.Models;
 
 namespace StudyConnect.Core.Interfaces;
+
+public interface ICommentRepository
+{
+    /// <summary>
+    /// Add an comment to a post or parent comment.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user.</param>
+    /// <param name="parentId">The unique identifier of the parent.</param>
+    /// <param name="comment">The model containing information about the comment.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<Guid?>> AddAsync(Guid userId, Guid parentId, ForumComment comment);
+
+    /// <summary>
+    /// Get a comment by its GUID.
+    /// </summary>
+    /// <param name="commentId">The unique identifier of the comment.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<ForumComment?>> GetByIdAsync(Guid commentId);
+
+    /// <summary>
+    /// Get all Comments of a specific post.
+    /// </summary>
+    /// <param name="postId">The unique identifier of the post.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<IEnumerable<ForumComment>?>> GetAllofPostAsync(Guid postId);
+
+    /// <summary
+    /// Updates a Comment by its GUID.
+    /// </summary>
+    /// <param name="commentId">The unique identifier of the comment.</param>
+    /// <param name="comment">A comment model containing the information for update.<param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<ForumComment?>> UpdateAsync(Guid commentId, ForumComment comment);
+
+    /// <summary>
+    /// Delete a comment by its GUID.
+    /// </summary> 
+    /// <param name="commentId">The unique identifier of the comment.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<bool>> DeleteAsync(Guid commentId);
+}
