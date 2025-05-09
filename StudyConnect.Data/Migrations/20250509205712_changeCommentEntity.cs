b55546dabe954804690a5ca3521946aada11e075
sokolgle <gleb.sokolovskiy@gmail.com>
2025-05-10 00:25:23 +0200

    fix: adjusted migrations [#67]

--- StudyConnect.Data/Migrations/20250509205712_changeCommentEntity.cs
+++ StudyConnect.Data/Migrations/20250509205712_changeCommentEntity.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace StudyConnect.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class changeComments : Migration
+    public partial class changeCommentEntity : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
