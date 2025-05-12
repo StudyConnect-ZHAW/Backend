47290bc9ab05aa89cdab94f8e08c90664b45bfff
Alex Sivapalan <sivapale@students.zhaw.ch>
2025-05-12 02:05:03 +0200

    Add TagController

---
+++ StudyConnect.API/Controllers/Tag/TagController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using StudyConnect.Data.Repositories;
+using StudyConnect.API.Dtos.Responses;
+
+namespace StudyConnect.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TagController : ControllerBase
+    {
+        private readonly TagRepository _tagRepository;
+
+        public TagController(TagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTags()
+        {
+            var tags = await _tagRepository.GetAllTagsAsync();
+            var tagDtos = tags.Select(t => new TagDto
+            {
+                Id = t.Id,
+                Name = t.Name,
+                Description = t.Description
+            });
+
+            return Ok(tagDtos);
+        }
+
+        [HttpPost]
+        public IActionResult PostNotImplemented()
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+    }
+}
