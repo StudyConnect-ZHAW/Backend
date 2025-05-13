03dab812a97beceb17c1350fdec7f3c83a4c80b4
Alex Sivapalan <sivapale@students.zhaw.ch>
2025-05-13 03:07:26 +0200

    Add TagDTO.cs file

---
+++ StudyConnect.API/Dtos/Responses/Tag/TagDto.cs
@@ -0,0 +1,9 @@
+namespace StudyConnect.API.Dtos.Responses
+{
+    public class TagDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
