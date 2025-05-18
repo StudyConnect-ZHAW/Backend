571ab384894eb28949539d5e0714035b06958c55
sokolgle <gleb.sokolovskiy@gmail.com>
2025-05-19 01:35:14 +0200

    feat: created ForumLike model [#97]

---
+++ StudyConnect.Core/Models/ForumLike.cs
@@ -0,0 +1,12 @@
+namespace StudyConnect.Core.Models;
+
+public class ForumLike
+{
+    public Guid LikeId { get; set; }
+
+    public Guid? ForumPostId { get; set; }
+
+    public Guid? ForumCommentId { get; set; }
+
+    public DateTime LikedAt { get; set; }
+}
