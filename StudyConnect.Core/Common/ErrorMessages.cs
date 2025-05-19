854ae8688b97b6f4a12d27219e8228a82784cd1a
sokolgle <gleb.sokolovskiy@gmail.com>
2025-05-19 02:24:16 +0200

    refactor: adjusted ErrorMessages class [#97]

--- StudyConnect.Core/Common/ErrorMessages.cs
+++ StudyConnect.Core/Common/ErrorMessages.cs
@@ -6,21 +6,29 @@ namespace StudyConnect.Core.Common
         public const string UnknownError = "An unexpected error occurred.";
         public const string NotFound = "The requested resource was not found.";
         public const string NotAuthorized = "Not authorized to perform this action.";
+        public const string NameRequired = "Name cannot be null, empty or whitespace.";
+        public const string TitleTaken = "Title already in use.";
+        public const string LikeExists = "Like already exists.";
+
+        public const string QueryFailure = "Malformed or invalid query.";
+
         public const string InvalidInput = "Input is invalid.";
 
         public const string InvalidUserId = "Invalid user ID.";
         public const string InvalidPostId = "Invalid post ID.";
         public const string InvalidCommentId = "Invalid comment ID.";
-        public const string CommentContentEmpty = "Comment content cannot be empty.";
-        public const string ParentCommentNotFound = "Parent comment not found.";
+        public const string InvalidCategoryId = "Invalid category ID.";
+        public const string CommentContentEmpty = "Missing comment content";
+        public const string PostContentEmpty = "Missing post content.";
 
-        public const string GeneralNotFound = "not found";
-        public const string CommentNotFound = "Comment not found.";
-        public const string CommentsNotFound = "Comments not found";
-        public const string PostNotFound = "Post not found.";
-        public const string UserNotFound = "User not found.";
+        public const string GeneralNotFound = "does not exist.";
+        public const string CategoryNotFound = "Category with specified ID does not exist.";
+        public const string CommentNotFound = "Comment with specified ID does not exist.";
+        public const string CommentsNotFound = "Comments with specified ID does not exist.";
+        public const string PostNotFound = "Post with specified ID does not exist.";
+        public const string UserNotFound = "User with specified ID does not exist.";
+        public const string ParentCommentNotFound = "Parent comment with specified ID does not exist.";
+        public const string LikeNotFound = "Like with specified ID does not exist.";
     }
 }
-
-
 
