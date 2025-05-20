f6dd5ea261a169edf8f892b5b71eb45bbd9cba1d
sokolgle <gleb.sokolovskiy@gmail.com>
2025-05-21 00:51:12 +0200

    feat: added GroupPost model && Interface [#99]

---
+++ StudyConnect.Core/Interfaces/IGroupPostRepository.cs
@@ -0,0 +1,49 @@
+using StudyConnect.Core.Common;
+using StudyConnect.Core.Models;
+
+namespace StudyConnect.Core.Interfaces;
+
+public interface IGroupPostRepository
+{
+    /// <summary>
+    /// Adds a new forum post to the database.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user who created the post.</param>
+    /// <param name="groupId">The unique identifier of group the post belogns to.</param>
+    /// <param name="post">The model containing information about the forum post.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<ForumPost>> AddAsync(Guid userId, Guid groupId, GroupPost? post);
+
+    /// <summary>
+    /// Retrieves a post by its unique identifier.
+    /// </summary>
+    /// <param name="postId">The unique identifier of the post.</param> 
+    /// <returns>An <see cref="OperationResult{T}"/> containing the post if found, or an error message if not.</returns>
+    Task<OperationResult<ForumPost?>> GetByIdAsync(Guid postId);
+
+    /// <summary>
+    /// Get all Post of the forum.
+    /// </summary>
+    /// <param name="groupId">the unique identifier of the group the post belogns to.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> containing a list of post if found, or an error message if not.</returns>
+    Task<OperationResult<IEnumerable<ForumPost>>> GetAllAsync(Guid groupId);
+
+    /// <summary>
+    /// Updates an existing post.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user who created the post.</param>
+    /// <param name="groupId">the unique identifier of the group the post belogns to.</param>
+    /// <param name="postId">The unique identifier of the post, that shoudl be updated.</param>
+    /// <param name="post">The model containing the information about the forum post for the update.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<ForumPost>> UpdateAsync(Guid userId, Guid groupId, Guid postId, GroupPost post);
+
+    /// <summary>
+    /// Deletes an exsiting post.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user who created the post.</param>
+    /// <param name="groupId">the unique identifier of the group the post belogns to.</param>
+    /// <param name="postId">The unique identifier of the post, that shoudl be deleted.</param>
+    /// <returns>An <see cref="OperationResult{T}"/> indicating success or failure.</returns>
+    Task<OperationResult<bool>> DeleteAsync(Guid userId, Guid groupId, Guid postId);
+}
---
+++ StudyConnect.Core/Models/GroupPost.cs
@@ -0,0 +1,18 @@
+namespace StudyConnect.Core.Models;
+
+public class GroupPost
+{
+    public required string Title { get; set; }
+
+    public required string? Content { get; set; }
+
+    public Guid GroupPostId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public Group? Group { get; set; }
+
+    public User? User { get; set; }
+}
