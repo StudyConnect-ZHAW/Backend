1e741ae16bc693fc17716ce8dde0534fd25c419d
sokolgle <gleb.sokolovskiy@gmail.com>
2025-05-22 21:41:34 +0200

    feat: migrations added [#97]

--- StudyConnect.Data/Migrations/20250522194025_AddLikes.Designer.cs
+++ StudyConnect.Data/Migrations/20250522194025_AddLikes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudyConnect.Data;
 
@@ -11,9 +12,11 @@ using StudyConnect.Data;
 namespace StudyConnect.Data.Migrations
 {
     [DbContext(typeof(StudyConnectDbContext))]
-    partial class StudyConnectDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250522194025_AddLikes")]
+    partial class AddLikes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
