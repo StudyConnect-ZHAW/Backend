63b1fe1758400c2a78d1ab6fe62bf5248bd52aee
sokolgle <gleb.sokolovskiy@gmail.com>
2025-05-29 23:59:25 +0200

    fix: renamed the User property in PostReadDto back to Author

--- StudyConnect.API/Dtos/Responses/Forum/PostReadDto.cs
+++ StudyConnect.API/Dtos/Responses/Forum/PostReadDto.cs
@@ -33,7 +33,7 @@ public class PostReadDto
     public int? LikeCount { get; set; }
 
     /// <summary>
-    /// The date and time when post was created. 
+    /// The date and time when post was created.
     /// </summary>
     public DateTime? Created { get; set; }
 
@@ -50,6 +50,5 @@ public class PostReadDto
     /// <summary>
     /// The creator of the post as a dto.
     /// </summary>
-    public UserReadDto? User { get; set; }
+    public UserReadDto? Author { get; set; }
 }
-
